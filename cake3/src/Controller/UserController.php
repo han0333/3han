08ec54d974a3f072fd8a268178e346f8547d3c40
han0333 <han03331@yahoo.co.jp>
2018-05-24 12:51:11 +0900

    変更

--- cake3/src/Controller/UserController.php
+++ cake3/src/Controller/UserController.php
@@ -3,11 +3,14 @@ namespace App\Controller;
 
 use App\Controller\AppController;
 use Cake\Routing\Router;
+use cake\ORM\TableRegistry;
+use Cake\Event\Event;
 
-class UserController extends AppController{
+class UsersController extends AppController{
 
     public function initialize(){
-        $this->viewBuilder()->setLayout('userLayout');
+        $this->viewBuilder()->setLayout('');
+        parent::initialize();
 
         //ベースURL
         $url = Router::url('/', true);
@@ -24,7 +27,7 @@ class UserController extends AppController{
         //ナビ項目セット
         $nav_list = array(
             "<li><a href=\"$url_c/select\">店舗一覧</a></li>",
-            "<li><a href=\"$url_c/rireki\">予約履歴</a></li>",
+            "<li><a href=\"$url_c/yoyaku\">予約履歴</a></li>",
             "<li><a href=\"$url_c/index\">ログアウト</a></li>"
         );
         $this->set("nav_list", $nav_list);
@@ -39,7 +42,6 @@ class UserController extends AppController{
         $link = "";
         $fab = "<a id='fab' fref='".$link."' class='btn-floating btn-large waves-effect waves-light red'>
         <i class='material-icons'>add</i></a>";
-        $fab= "";
         $this->set("fab", $fab);
 
         //ヘッドライン
@@ -51,26 +53,62 @@ class UserController extends AppController{
     }
     //ログイン
     public function index(){
+        $this->viewBuilder()->setLayout('userLayout');
         $this->set("header_flag", 0);
-       //ヘッドライン
+        //ヘッドライン
+        if ($this->request->is('post')) {
+            $user = $this->Auth->identify();
+            if ($user) {
+                $this->Auth->setUser($user);
+                return $this->redirect($this->Auth->redirectUrl());
+            }
+            $this->Flash->error(__('Invalid username or password, try again'));
+        }
     }
     //登録
     public function register(){
+        $this->Users = TableRegistry::get('Users');
+        $this->Userroles = TableRegistry::get('Userroles');
+        $this->viewBuilder()->setLayout('userLayout');
         $this->set("header_flag", 0);
         //ヘッドライン
         $this->set("headline", "<h4>新規登録</h4>");
+        $user = $this->Users->find('all');
+        $this->set('user',$user);
+        $this->set('entity',$this->Users->newEntity());
+
+        $role = $this->Userroles->find('all');
+        $this->set('role',$role);
+        $this->set('entity',$this->Userroles->newEntity());
+
+        $user = $this->Users->newEntity();
+        if ($this->request->is('post')) {
+            $user = $this->Users->patchEntity($user, $this->request->getData());
+            if ($this->Users->save($user)) {
+                $this->Flash->success(__('The user has been saved.'));
+
+                //ユーザロール登録
+                $user = $this->Userroles->newEntity($this->request->data);
+                $name = $user->user_id;
+                $datas = ['user_id'=>$name,'role_id'=>2];
+
+                $data = $this->Userroles->newEntity($datas);
+
+                $this->Userroles->save($data);
+            }
+            $this->Flash->error(__('The user could not be saved. Please, try again.'));
+        }
+        $this->set(compact('user'));
+        //$this->set('_serialize', ['user']);
+
     }
+
     //パスワードリセット
     public function reset(){
+        $this->viewBuilder()->setLayout('userLayout');
         $this->set("header_flag", 0);
         //ヘッドライン
         $this->set("headline", "<h4>パスワードリセット</h4>");
-    }
-    //パスワード再設定画面
-    public function repassword(){
-        $this->set("header_flag", 0);
-        //ヘッドライン
-        $this->set("headline", "<h4>パスワード再設定</h4>");
     }
     //店舗選択
     public function select(){
@@ -80,20 +118,38 @@ class UserController extends AppController{
         $this->Shoplists = TableRegistry::get('lunchshops');
         $data=$this->Shoplists->find('all');
         $this->set('result', $data);
+
     }
     //予約履歴
     public function rireki(){
+        $this->Orders = TableRegistry::get('Orders');
+        $this->Products = TableRegistry::get('Products');
+        $this->viewBuilder()->setLayout('userLayout');
         //ヘッドライン
-        $this->set("headline", "<h4>予約履歴</h4>");
+        $this->set("headline", "<h4></h4>");
+        $data = $this->Orders->find('all')->contain('Products');
+        $this->set('data',$data);
+        $this->set('entity',$this->Orders->newEntity());
+
+        $product = $this->Products->find('all');
+        $this->set('product',$product);
+        $this->set('entity',$this->Products->newEntity());
 
     }
     //弁当選択
     public function bento(){
+        $this->viewBuilder()->setLayout('userLayout');
         //ヘッドライン
         $this->set("headline", "<h4>弁当一覧</h4>");
 
-    }
+        $shop_id = $this->request->data('shop_id');
+        /*
+        $this->set('entity',$this->Shoplists->newEntity());
+        $entity = $this->Shoplists->get($value);
+        $this->set('entity',$entity);
+        */
 
+    }
     //予約
     public function yoyaku1(){
         //ヘッドライン
@@ -101,8 +157,20 @@ class UserController extends AppController{
         $this->Shoplists = TableRegistry::get('products');
         $data=$this->Shoplists->find('all');
         $this->set('result1', $data);
-
     }
+
+    //予約キャンセル機能(画面不要)
+    public function delRecord($id){
+        if($id != null){
+            try{
+                $entity = $this->Costs->get($id);
+                $this->Costs->delete($entity);
+            }catch(Exception $e){
+                Log::write('debug', $e->getMessage());
+            }
+        }
+        return $this->redirect(['action' => 'index']);
+	}
 
     //弁当予約
     public function yoyaku2(){
@@ -111,4 +179,9 @@ class UserController extends AppController{
 
     }
 
+    public function beforeFilter(Event $event)
+    {
+        parent::beforeFilter($event);
+        $this->Auth->allow(['index','register','reset']);
+    }
 }
