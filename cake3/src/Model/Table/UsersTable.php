cd0faa6f2d652eec01cbf020649d19a4493c9642
han0333 <39038705+han0333@users.noreply.github.com>
2018-05-24 12:34:33 +0900

    pass

--- cake3/src/Model/Table/UsersTable.php
+++ cake3/src/Model/Table/UsersTable.php
@@ -6,38 +6,33 @@ use Cake\ORM\RulesChecker;
 use Cake\ORM\Table;
 use Cake\Validation\Validator;
 
-class ShoplistsTable extends Table{
-
+class UsersTable extends Table {
     public function initialize(array $config){
-        $this->belongsTo('Testusers');
-        $this->belongsTo('shops');
-        $this->belongsTo('product');
+        parent::initialize($config);
     }
 
     public function validationDefault(Validator $validator){
-        $validator->integer('id');
         $validator
-            ->add('name','custom',[
+            ->requirePresence('user_name')
+            ->notEmpty('user_name','必須項目です')
+            ->maxLength('user_name',16,'16文字以内で入力してください。');
+        $validator
+            ->add('user_id','custom',[
                 'rule'=>['custom',"/^[0-9a-z]+$/"],
                 'message'=>__('英数字で入力してください'),
             ])
-            ->notEmpty('username')
-            ->minLength('username',6,'6文字以上で入力してください。')
-            ->maxLength('username',20,'20文字以内で入力してください。');
-        $validator
-            ->add('display_name','custom',[
-                'rule'=>['custom',"/^[0-9a-z]+$/"],
-                'message'=>__('英数字で入力してください'),
-            ])
-            ->notEmpty('display_name')
+            ->notEmpty('user_id')
             ->minLength('display_name',6,'6文字以上で入力してください。')
-            ->maxLength('display_name',20,'20文字以内で入力してください。');
+            ->maxLength('display_name',16,'20文字以内で入力してください。');
         $validator
             ->add('password','custom',[
                 'rule'=>['custom',"/^[0-9a-z]+$/"],
                 'message'=>__('英数字で入力してください'),
             ])
+            ->notEmpty('password')
             ->minLength('password',8,'8文字以上で入力してください。')
             ->maxLength('password',16,'16文字以内で入力してください。');
+        return $validator;
     }
+
 }
