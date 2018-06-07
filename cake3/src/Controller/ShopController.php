e34c498dc95523ed290fc4d027a53fbf96c230bd
han0333 <39038705+han0333@users.noreply.github.com>
2018-06-07 19:18:13 +0900

    shop index
    ordersテーブルのnumber列削除に伴う変更

--- cake3/src/Controller/ShopController.php
+++ cake3/src/Controller/ShopController.php
@@ -3,81 +3,80 @@ namespace App\Controller;
 
 use App\Controller\AppController;
 use Cake\Routing\Router;
+use Cake\Event\Event;
+use cake\ORM\TableRegistry;
+use Cake\I18n\Time;
+use Cake\Datasource\ConnectionManager;
 
 class ShopController extends AppController{
-    
-    public function initialize(){
-        $this->viewBuilder()->setLayout('shopLayout');
 
-        //ベースURL
+    public function initialize(){
+        $this->viewBuilder()->setLayout('');
+        parent::initialize();
         $url = Router::url('/', true);
         $this->set("url", $url);
 
-        //ベースURL + コントローラー名
-        $controllerName = $this->name;
-        $url_c = "$url"."$controllerName";
-        $this->set("url_c", $url_c);
-
-        //タイトルセット
-        $this->set("title", "弁当予約システム");
-
-        //ナビ項目セット
-        $nav_list = array(
-            "<li><a href=\"$url_c/\">売上確認</a></li>",
-            "<li><a href=\"$url_c/list\">弁当一覧</a></li>",
-            "<li><a href=\"$url_c/yoyaku\">予約確認</a></li>",
-            "<li><a href=\"$url/User/index\">ログアウト</a></li>"
-        );
-        $this->set("nav_list", $nav_list);
-
-        //ヘッダーの有無　デフォルト”１”で表示
-        $this->set("header_flag", 1);
-
-        //ヘッダーカラー
-        $this->set("color", "teal lighten-2");
-
-        //FAB
-        $link = "";
-        $fab = "<a id='fab' fref='".$link."' class='btn-floating btn-large waves-effect waves-light red'>
-        <i class='material-icons'>add</i></a>";
-        $fab= "";
-        $this->set("fab", $fab);
-
-        //ヘッドライン
-        $this->set("headline", "<h4></h4>");
-        $btn_class1 = "waves-effect waves-light btn-large w-100 my-5";
-
-        //ボタンクラス
-        $this->set("btn_class1", $btn_class1);
-         
     }
 
     public function index(){
-         $this->set("headline", "<h4>売上確認</h4>");
+        $this->Orders = TableRegistry::get('Orders');
+        $this->Products = TableRegistry::get('Products');
+        $this->viewBuilder()->setLayout('shopLayout');
+
+        $name = $this->request->session()->read('Auth');
+        $id = $name['User']['user_id'];
+        $order = $this->Orders->find('all',['conditions'=>['Orders.lunchshop_user_id'=>$id]])->contain('Products')->group('Orders.product_id');
+        //$order->select(['count' => $order->func()->sum('number'),'Orders.product_id','Orders.date','Products.price','Products.product_name'])->group('Orders.product_id');
+        $this->set('order',$order);
+        $this->set('entity',$this->Orders->newEntity());
+
+
+        $connection = ConnectionManager::get('default');
+        $con = "SELECT products.product_name,count(*) * products.price as sale FROM orders LEFT JOIN products ON orders.product_id = products.product_id WHERE orders.lunchshop_user_id = '$id' GROUP BY orders.product_id";
+        $count = $connection->execute($con)->fetchAll('assoc');
+        $this->set('count',$count);
+
+
+
+        $product = $this->Products->find('all');
+        $this->set('product',$product);
+        $this->set('entity',$this->Products->newEntity());
+
+        $date = Time::now('Asia/Tokyo');
+        $this->set('date',$date);
+
+
+        if($_POST) {
+        $y = $_POST['year'];
+        $m = $_POST['month'];
+        $d = $_POST['day'];
+
+        $connection = ConnectionManager::get('default');
+        $sql = "SELECT products.product_name,count(*) * products.price AS sale FROM orders left outer join products on orders.product_id = products.product_id WHERE date = '$y-$m-$d' and orders.lunchshop_user_id = '$id' GROUP BY orders.product_id  ";
+        $result = $connection->execute($sql)->fetchAll('assoc');
+        $this->set('result',$result);
+
+
+        }
+
     }
     public function list(){
-        $this->set("headline", "<h4>弁当一覧</h4>");
-        //ベースURL
-        $url = Router::url('/', true);
-
-        //ベースURL + コントローラー名
-        $controllerName = $this->name;
-        $url_c = "$url"."$controllerName";
-
-        //FAB
-        $link = "$url_c"."/add";
-        $fab = "<a id='fab' href='".$link."' class='btn-floating btn-large waves-effect waves-light red'>
-        <i class='material-icons'>add</i></a>";
-        $this->set("fab", $fab);
+        $this->viewBuilder()->setLayout('shopLayout');
     }
     public function yoyaku(){
-        $this->set("headline", "<h4>予約詳細</h4>");
+        $this->viewBuilder()->setLayout('shopLayout');
     }
     public function edit(){
-        $this->set("headline", "<h4>弁当編集</h4>");
+        $this->viewBuilder()->setLayout('shopLayout');
     }
     public function add(){
-        $this->set("headline", "<h4>弁当追加</h4>");
+        $this->viewBuilder()->setLayout('shopLayout');
+    }
+
+    public function beforeFilter(Event $event)
+    {
+        parent::beforeFilter($event);
+        $this->Auth->deny();
     }
 
 
--- cake3/src/Template/Shop/index.ctp
+++ cake3/src/Template/Shop/index.ctp
@@ -35,7 +35,7 @@ if ($_POST) {
                     data:';
                     echo '[';
                     foreach($result as $obj):
-                    echo '"'.$obj['price'] * $obj['number'],'",';
+                    echo '"'.$obj['sale'],'",';
                     endforeach;
                     echo '],
                     backgroundColor: ["rgb(255, 99, 132)", "rgb(54, 162, 235)", "rgb(255, 205, 86)"]
@@ -43,12 +43,15 @@ if ($_POST) {
             }
         });
     </script>';
+    } else {
+        echo 'この日の売り上げはありません。';
     }
 
-} else {
+
+} else if(!$_POST) {
 
     echo '<form action="shop" method="post">';
-        echo '<input type="number" value='.$year.' name="year">年';
+        echo '<input type="number" value='.$year.' name="year" min=2018>年';
         echo '<input type="number" value='.$month.' max=12 min=1 name="month">月';
         echo '<input type="number" value='.$day.' max=31 min=1 name="day">日';
         echo '<button type="submit" class="btn-primary">確認</button>';
@@ -63,15 +66,15 @@ if ($_POST) {
             data: {
                 labels:';
                     echo '[';
-                    foreach($order as $obj):
-                    echo '"'.$obj->product->product_name,'",';
+                    foreach($count as $obj):
+                    echo '"'.$obj['product_name'],'",';
                     endforeach;
                     echo '],
                 datasets: [{
                     data:';
                     echo '[';
-                    foreach($order as $obj):
-                    echo '"'.$obj->product->price * $obj->number,'",';
+                    foreach($count as $obj):
+                    echo '"'.$obj['sale'],'",';
                     endforeach;
                     echo '],
                     backgroundColor: ["rgb(255, 99, 132)", "rgb(54, 162, 235)", "rgb(255, 205, 86)"]
@@ -81,29 +84,3 @@ if ($_POST) {
     </script>';
 }
 ?>
-
-<!--<canvas id="myChart"></canvas>
-<script>
-
-        new Chart(document.getElementById("myChart"), {
-            type: "doughnut",
-            data: {
-                labels: <?php
-                    echo '[';
-                    foreach($order as $obj):
-                    echo '"'.$obj->product->product_name,'",';
-                    endforeach;
-                    echo ']'?>,
-                datasets: [{
-                    data: <?php
-                    echo '[';
-                    foreach($order as $obj):
-                    echo '"'.$obj->product->price * $obj->number,'",';
-                    endforeach;
-                    echo ']'?>,
-                    backgroundColor: ["rgb(255, 99, 132)", "rgb(54, 162, 235)", "rgb(255, 205, 86)"]
-                }]
-            }
-        });
-}
-</script>-->
