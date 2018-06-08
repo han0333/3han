762858279e379678c26b2a1ee2592959909aa719
han0333 <39038705+han0333@users.noreply.github.com>
2018-06-08 11:18:49 +0900

    re

--- cake3/src/Controller/ShopController.php
+++ cake3/src/Controller/ShopController.php
@@ -3,82 +3,235 @@ namespace App\Controller;
 
 use App\Controller\AppController;
 use Cake\Routing\Router;
-use Cake\Event\Event;
-use cake\ORM\TableRegistry;
-use Cake\I18n\Time;
-use Cake\Datasource\ConnectionManager;
+<<<<<<< HEAD
 
 class ShopController extends AppController{
+    
+=======
+use Cake\ORM\TableRegistry;
 
+class ShopController extends AppController{
+>>>>>>> 17fb17aada2ca580ae1096baceae0329aecf20c9
     public function initialize(){
-        $this->viewBuilder()->setLayout('');
-        parent::initialize();
+        $this->viewBuilder()->setLayout('shopLayout');
+
+        //ベースURL
         $url = Router::url('/', true);
         $this->set("url", $url);
 
+        //ベースURL + コントローラー名
+        $controllerName = $this->name;
+        $url_c = "$url"."$controllerName";
+        $this->set("url_c", $url_c);
+
+        //タイトルセット
+        $this->set("title", "弁当予約システム");
+
+        //ナビ項目セット
+        $nav_list = array(
+            "<li><a href=\"$url_c/\">売上確認</a></li>",
+            "<li><a href=\"$url_c/list\">弁当一覧</a></li>",
+            "<li><a href=\"$url_c/yoyaku\">予約確認</a></li>",
+            "<li><a href=\"$url/User/index\">ログアウト</a></li>"
+        );
+        $this->set("nav_list", $nav_list);
+
+        //ヘッダーの有無　デフォルト”１”で表示
+        $this->set("header_flag", 1);
+
+        //ヘッダーカラー
+        $this->set("color", "teal lighten-2");
+
+        //FAB
+        $link = "";
+        $fab = "<a id='fab' fref='".$link."' class='btn-floating btn-large waves-effect waves-light red'>
+        <i class='material-icons'>add</i></a>";
+        $fab= "";
+        $this->set("fab", $fab);
+
+        //ヘッドライン
+        $this->set("headline", "<h4></h4>");
+        $btn_class1 = "waves-effect waves-light btn-large w-100 my-5";
+
+        //ボタンクラス
+        $this->set("btn_class1", $btn_class1);
+         
     }
 
     public function index(){
-        $this->Orders = TableRegistry::get('Orders');
-        $this->Products = TableRegistry::get('Products');
-        $this->viewBuilder()->setLayout('shopLayout');
-
-        $name = $this->request->session()->read('Auth');
-        $id = $name['User']['user_id'];
-        $order = $this->Orders->find('all',['conditions'=>['Orders.lunchshop_user_id'=>$id]])->contain('Products')->group('Orders.product_id');
-        //$order->select(['count' => $order->func()->sum('number'),'Orders.product_id','Orders.date','Products.price','Products.product_name'])->group('Orders.product_id');
-        $this->set('order',$order);
-        $this->set('entity',$this->Orders->newEntity());
-
-
-        $connection = ConnectionManager::get('default');
-        $con = "SELECT products.product_name,count(*) * products.price as sale FROM orders LEFT JOIN products ON orders.product_id = products.product_id WHERE orders.lunchshop_user_id = '$id' GROUP BY orders.product_id";
-        $count = $connection->execute($con)->fetchAll('assoc');
-        $this->set('count',$count);
-
-
-
-        $product = $this->Products->find('all');
-        $this->set('product',$product);
-        $this->set('entity',$this->Products->newEntity());
-
-        $date = Time::now('Asia/Tokyo');
-        $this->set('date',$date);
-
-
-        if($_POST) {
-        $y = $_POST['year'];
-        $m = $_POST['month'];
-        $d = $_POST['day'];
-
-        $connection = ConnectionManager::get('default');
-        $sql = "SELECT products.product_name,count(*) * products.price AS sale FROM orders left outer join products on orders.product_id = products.product_id WHERE date = '$y-$m-$d' and orders.lunchshop_user_id = '$id' GROUP BY orders.product_id  ";
-        $result = $connection->execute($sql)->fetchAll('assoc');
-        $this->set('result',$result);
-
-
-        }
-
+         $this->set("headline", "<h4>売上確認</h4>");
+<<<<<<< HEAD
     }
     public function list(){
-        $this->viewBuilder()->setLayout('shopLayout');
+        $this->set("headline", "<h4>弁当一覧</h4>");
+        //ベースURL
+        $url = Router::url('/', true);
+
+        //ベースURL + コントローラー名
+        $controllerName = $this->name;
+        $url_c = "$url"."$controllerName";
+
+        //FAB
+        $link = "$url_c"."/add";
+        $fab = "<a id='fab' href='".$link."' class='btn-floating btn-large waves-effect waves-light red'>
+        <i class='material-icons'>add</i></a>";
+        $this->set("fab", $fab);
     }
+=======
+    }
+    public function list(){
+        $this->set("headline", "<h4>弁当一覧</h4>");
+        //ベースURL
+        $url = Router::url('/', true);
+
+        //ベースURL + コントローラー名
+        $controllerName = $this->name;
+        $url_c = "$url"."$controllerName";
+
+        //FAB
+        $link = "$url_c"."/add";
+        $fab = "<a id='fab' href='".$link."' class='btn-floating btn-large waves-effect waves-light red'>
+        <i class='material-icons'>add</i></a>";
+        $this->set("fab", $fab);
+
+
+    
+        /*
+        $name = $this->request->session()->read('Auth'); 
+            echo $name['lunshop_user_id']['lunchshop_name'];
+        */
+        
+
+        
+        $this->viewBuilder()->setLayout('shopLayout');
+
+        //データベース受け取り
+        $this->products = TableRegistry::get('products');
+        $data = $this->products->find('all');
+        $this->set('data',$data);
+
+        if(!empty($this->request->data)) {
+            //$name変数には「user1」が格納される
+            $product_name = $this->request->data('product_name');
+            $price = $this->request->data('price');  
+            $image = $this->image->find('all');
+            $this->set(compact('image'));
+            // 以下保存ロジック
+            $this->products = TableRegistry::get('products');
+   
+        }
+
+
+        /*参考
+        $this->set('Entity', $this->products->newEntity());
+        if($this->request->is('post')){
+            $data = $this->products->find('all',['conditions'=>[
+                'article like'=>"%{$this->request->data['search']}%"]]);
+        }else{
+            $data = $this->products->find('all');
+        }
+        $data->order(['products_id'=>'DESC']);
+        $this->set('data', $data);
+         */
+
+
+
+
+    }
+>>>>>>> 17fb17aada2ca580ae1096baceae0329aecf20c9
     public function yoyaku(){
-        $this->viewBuilder()->setLayout('shopLayout');
+        $this->set("headline", "<h4>予約詳細</h4>");
     }
+    
     public function edit(){
-        $this->viewBuilder()->setLayout('shopLayout');
+        $this->set("headline", "<h4>弁当編集</h4>");
+<<<<<<< HEAD
     }
     public function add(){
+        $this->set("headline", "<h4>弁当追加</h4>");
+=======
         $this->viewBuilder()->setLayout('shopLayout');
+        /*$this->set('Entity', $this->Shop->newEntity());
+
+        $product_id = $this->set->request('product_id');
+        */
+        //id取得
+        $this->products = TableRegistry::get('products');
+        $ben_id =$_GET['id'];
+        $data=$this->products->find('all') 
+        ->where(['product_id' => $ben_id])->all();
+        $this->set('result',$data);
+
+    
+
+    
+             
+     }
+     //弁当編集機能
+     public function editRecord(){
+       $this->products = TableRegistry::get('products');
+        if (!empty($this->request->is('post'))) {
+            //$entity = $this->products->newEntity();
+            $entity = $this->products->get($this->request->data['product_id']);
+            $entity->product_name = $this->request->data['product_name'];
+            $entity->price = $this->request->data['price'];
+            $entity->image = file_get_contents($this->request->data['image']['tmp_name']);
+                if($this->products->save($entity)){
+                    $this->set('msg',"成功しました" . $this->request->data['image']['tmp_name']);
+        //登録に成功したときの処理
+                }
+            }
+        
+        /*   try{
+               $Entity = $this->products->get($this->request->data['product_id']);
+               $this->products->patchEntity($Entity, $this->request->data);
+               $this->products->save($Entity);
+           }catch(Exception $e){
+               Log::write('debug', $e->getMessage());
+           }
+        
+      return $this->redirect(['action' => 'list']);
+         */ 
     }
+    
 
-    public function beforeFilter(Event $event)
-    {
-        parent::beforeFilter($event);
-        $this->Auth->deny();
+     //弁当編集削除機能
+     public function delRecord(){
+        if($this->request->is('post')){
+            try{
+                $entity = $this->products->get($this->request->data['product_id']);
+                $this->products->delete($entity);
+            }catch(Exception $e){
+                Log::write('debug', $e->getMessage());
+            }
+        }
+        return $this->redirect(['action' => 'list']);
     }
+    
+    public function add(){
+        $this->set("headline", "<h4>弁当追加</h4>");
+        
+        $this->products = TableRegistry::get('products');
+
+        $user_name = $this->request->session()->read('Auth');  
+        $id = $user_name['User']['user_id'];
+        
+        if (!empty($this->request->is('post'))) {
+            $entity = $this->products->newEntity();
+            $entity->lunchshop_user_id = $id;
+            $entity->product_name = $this->request->data['product_name'];
+            $entity->price = $this->request->data['price'];
+
+            if($this->request->data['contents']['name'] != ""){
+                $entity->image = file_get_contents($this->request->data['contents']['tmp_name']);
+            }   
+            if($this->products->save($entity)){
+                $this->set('msg',"成功しました" . $this->request->data['image']['tmp_name']);
+            }
+        }
+>>>>>>> 17fb17aada2ca580ae1096baceae0329aecf20c9
+    }
+}        
 
 
-}
-
+    
